add4fcfd896781f2b4bf8e1f62271813810c1ebc
Álvaro Cañas <acaygo@gmail.com>
2025-01-29 09:06:16 +0100

    css y tablero invisible

--- app/page.js
+++ app/page.js
@@ -1,15 +1,33 @@
 import styles from "./page.module.css";
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
-}
-
-export default function Home() {
+export default function Name() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Álvaro Cañas González</h1>
       </main>
     </div>
+  );
+}
+
+export function Square() {
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
   );
 }
